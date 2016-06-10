db4869da2793b55855c679db8d5ee4705c122be3
Danilo F. Silva <danilofes@gmail.com>
2016-06-10 09:57:45 -0300

    Implement template analyzedCommits

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
 var app = angular.module('wwr', [
   'ngRoute'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  //$locationProvider.hashPrefix('!');
-
   $routeProvider.
     when('/', {templateUrl: 'home.html'}).
     when('/projects', {templateUrl: 'projects.html'}).
-    when('/analyzedCommits', {templateUrl: 'analyzedCommits.html'}).
+    when('/analyzedCommits', {templateUrl: 'analyzedCommits.html', controller: 'analyzedCommitsCtrl'}).
     when('/allCommits', {templateUrl: 'allCommits.html'}).
     when('/thematicAnalysis', {templateUrl: 'thematicAnalysis.html'}).
     otherwise({redirectTo: '/'});
-
-  //$routeProvider.otherwise({redirectTo: '/view1'});
 }]);
 
-//app.controller('View2Ctrl', [function() {
 
-//}]);
+app.controller('analyzedCommitsCtrl', function analyzedCommitsCtrl($scope) {
+  $scope.commits = [
+    {
+        "id": 1105586,
+        "repository": "https://github.com/SecUpwN/Android-IMSI-Catcher-Detector.git",
+        "sha1": "e235f884f2e0bc258da77b9c80492ad33386fa86",
+        "author": "Marvin Arnold",
+        "message": "Refactored AIMSICDDbAdapter::DbHelper#onCreate for easier reading\n",
+        "motivations": [
+            "EM: Decompose method to improve readability"
+        ],
+        "others": [
+            "Unknown",
+            "ide: Unknown"
+        ]
+    }];
+});
