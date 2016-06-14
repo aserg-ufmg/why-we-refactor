b68075c9922f205f031154a3b869254584fd89be
Danilo F. Silva <danilofes@gmail.com>
2016-06-14 01:46:21 -0300

    Create commit details view

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ app.filter("sanitize", ['$sce', function($sce) {
 app.controller('projects', function projects($scope, $http) {
   $http({
     method: 'GET',
-    url: '/projects.json'
+    url: 'projects.json'
   }).then(function successCallback(response) {
     $scope.projects = response.data;
   });
@@ -32,7 +32,7 @@ app.controller('projects', function projects($scope, $http) {
 app.controller('thematicAnalysis', function thematicAnalysis($scope, $http) {
   $http({
     method: 'GET',
-    url: '/thematicAnalysis.json'
+    url: 'thematicAnalysis.json'
   }).then(function successCallback(response) {
     $scope.commits = response.data;
   });
@@ -41,7 +41,7 @@ app.controller('thematicAnalysis', function thematicAnalysis($scope, $http) {
 app.controller('allCommits', function allCommits($scope, $http) {
   $http({
     method: 'GET',
-    url: '/refactorings.json'
+    url: 'refactorings.json'
   }).then(function successCallback(response) {
     $scope.commits = response.data;
   });
@@ -50,7 +50,7 @@ app.controller('allCommits', function allCommits($scope, $http) {
 app.controller('commit', function commit($scope, $http, $routeParams) {
   $http({
     method: 'GET',
-    url: '/commitMap.json'
+    url: 'commitMap.json'
   }).then(function successCallback(response) {
     $scope.commit = response.data[$routeParams.commitId];
   });
