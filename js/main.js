4c3ce733a1d8b2b8c28838eed0a89d1cbb8f0c11
Danilo F. Silva <danilofes@gmail.com>
2016-06-14 01:44:06 -0300

    Create commit details view

--- js/main.js
+++ js/main.js
@@ -8,9 +8,16 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
     when('/', {templateUrl: 'home.html'}).
     when('/projects', {templateUrl: 'projects.html', controller: 'projects'}).
     when('/analyzedCommits', {templateUrl: 'analyzedCommits.html', controller: 'thematicAnalysis'}).
-    when('/allCommits', {templateUrl: 'allCommits.html'}).
+    when('/allCommits', {templateUrl: 'allCommits.html', controller: 'allCommits'}).
     when('/thematicAnalysis', {templateUrl: 'thematicAnalysis.html', controller: 'thematicAnalysis'}).
+    when('/analyzedCommits/:commitId', {templateUrl: 'commit.html', controller: 'commit'}).
     otherwise({redirectTo: '/'});
+}]);
+
+app.filter("sanitize", ['$sce', function($sce) {
+  return function(htmlCode){
+    return $sce.trustAsHtml(htmlCode);
+  }
 }]);
 
 app.controller('projects', function projects($scope, $http) {
@@ -28,5 +35,23 @@ app.controller('thematicAnalysis', function thematicAnalysis($scope, $http) {
     url: '/thematicAnalysis.json'
   }).then(function successCallback(response) {
     $scope.commits = response.data;
+  });
+});
+
+app.controller('allCommits', function allCommits($scope, $http) {
+  $http({
+    method: 'GET',
+    url: '/refactorings.json'
+  }).then(function successCallback(response) {
+    $scope.commits = response.data;
+  });
+});
+
+app.controller('commit', function commit($scope, $http, $routeParams) {
+  $http({
+    method: 'GET',
+    url: '/commitMap.json'
+  }).then(function successCallback(response) {
+    $scope.commit = response.data[$routeParams.commitId];
   });
 });
