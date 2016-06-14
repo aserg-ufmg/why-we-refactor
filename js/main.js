85df619b0597ce910d63bfe90cb4505d813c95be
Danilo F. Silva <danilofes@gmail.com>
2016-06-13 23:33:43 -0300

    Create thematicAnalysis view

--- js/main.js
+++ js/main.js
@@ -6,28 +6,27 @@ var app = angular.module('wwr', [
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $routeProvider.
     when('/', {templateUrl: 'home.html'}).
-    when('/projects', {templateUrl: 'projects.html'}).
-    when('/analyzedCommits', {templateUrl: 'analyzedCommits.html', controller: 'analyzedCommitsCtrl'}).
+    when('/projects', {templateUrl: 'projects.html', controller: 'projects'}).
+    when('/analyzedCommits', {templateUrl: 'analyzedCommits.html', controller: 'thematicAnalysis'}).
     when('/allCommits', {templateUrl: 'allCommits.html'}).
-    when('/thematicAnalysis', {templateUrl: 'thematicAnalysis.html'}).
+    when('/thematicAnalysis', {templateUrl: 'thematicAnalysis.html', controller: 'thematicAnalysis'}).
     otherwise({redirectTo: '/'});
 }]);
 
+app.controller('projects', function projects($scope, $http) {
+  $http({
+    method: 'GET',
+    url: '/projects.json'
+  }).then(function successCallback(response) {
+    $scope.projects = response.data;
+  });
+});
 
-app.controller('analyzedCommitsCtrl', function analyzedCommitsCtrl($scope) {
-  $scope.commits = [
-    {
-        "id": 1105586,
-        "repository": "https://github.com/SecUpwN/Android-IMSI-Catcher-Detector.git",
-        "sha1": "e235f884f2e0bc258da77b9c80492ad33386fa86",
-        "author": "Marvin Arnold",
-        "message": "Refactored AIMSICDDbAdapter::DbHelper#onCreate for easier reading\n",
-        "motivations": [
-            "EM: Decompose method to improve readability"
-        ],
-        "others": [
-            "Unknown",
-            "ide: Unknown"
-        ]
-    }];
+app.controller('thematicAnalysis', function thematicAnalysis($scope, $http) {
+  $http({
+    method: 'GET',
+    url: '/thematicAnalysis.json'
+  }).then(function successCallback(response) {
+    $scope.commits = response.data;
+  });
 });
